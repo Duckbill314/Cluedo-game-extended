b1612dff926af235c7f4d493fbb8ce63c2748971
Mazen Khallaf <khallamaze@ecs.vuw.ac.nz>
2023-08-28 15:56:01 +1200

    Refactored Character class

--- Java/Character.java
+++ Java/Character.java
@@ -1,7 +1,18 @@
-public class Character extends Item
-{
-public Character(String aName, String aDisplayIcon, int aX, int aY)
-  {
+/**
+ * The `Character` class represents a character in a game.
+ * It extends the `Item` class, inheriting properties such as name, display icon, and position.
+ */
+public class Character extends Item {
+
+  /**
+   * Constructor for the Character class.
+   *
+   * @param aName         The name of the character.
+   * @param aDisplayIcon  The display icon or image representing the character.
+   * @param aX            The X-coordinate position of the character.
+   * @param aY            The Y-coordinate position of the character.
+   */
+  public Character(String aName, String aDisplayIcon, int aX, int aY) {
     super(aName, aDisplayIcon, aX, aY);
   }
 }
