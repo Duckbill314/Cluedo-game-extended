2289d337c24ce41a4d2e38031792ca317d8dc83a
Mazen Khallaf <khallamaze@ecs.vuw.ac.nz>
2023-08-28 15:54:21 +1200

    Fixed up commenting for Card class

--- Java/Card.java
+++ Java/Card.java
@@ -1,56 +1,106 @@
-public class Card
-{
+/**
+ * The `Card` class represents a card in Hobby Detective.
+ * It can be a murder card or a regular card, and can be either an estate card, a weapon card, or a character card.
+ *
+ * @author William Huang
+ */
+public class Card {
 
-  private boolean isMurder;
-  private Player owner;
-  private String name;
-  private String type;
+    /**
+     * Indicates whether this card is one of the three murder cards.
+     */
+    private boolean isMurder;
 
-  public Card(boolean aIsMurder, Player aOwner, String aName,String aType)
-  {
-    isMurder = aIsMurder;
-    owner = aOwner;
-    name = aName;
-    type = aType;
-  }
+    /**
+     * The player who holds this card.
+     */
+    private Player owner;
 
-  public void setOwner(Player aOwner) {
-    owner = aOwner;
-  }
+    /**
+     * The name of the card.
+     */
+    private final String name;
 
-  public void setIsMurder(boolean aIsMurder) {
-    isMurder = aIsMurder;
-  }
+    /**
+     * The type of the card (character, estate, weapon).
+     */
+    private final String type;
 
-  /**
-   * is this card one of the three murder cards?
-   */
-  public boolean getIsMurder()
-  {
-    return isMurder;
-  }
+    /**
+     * Constructor for the Card class.
+     *
+     * @param aIsMurder Indicates whether this card is a murder card.
+     * @param aOwner    The player who owns this card.
+     * @param aName     The name of the card.
+     * @param aType     The type or category of the card.
+     */
+    public Card(boolean aIsMurder, Player aOwner, String aName, String aType) {
+        isMurder = aIsMurder;
+        owner = aOwner;
+        name = aName;
+        type = aType;
+    }
 
-  /**
-   * the player who holds the card
-   */
-  public Player getOwner()
-  {
-    return owner;
-  }
+    /**
+     * Sets the owner of the card.
+     *
+     * @param aOwner The player who will be the new owner of the card.
+     */
+    public void setOwner(Player aOwner) {
+        owner = aOwner;
+    }
 
-  public String getName()
-  {
-    return name;
-  }
+    /**
+     * Sets whether this card is one of the three murder cards.
+     *
+     * @param aIsMurder true if this card is a murder card; otherwise, false.
+     */
+    public void setIsMurder(boolean aIsMurder) {
+        isMurder = aIsMurder;
+    }
 
-   public String getType()
-  {
-    return type;
-  }
+    /**
+     * Checks if this card is one of the three murder cards.
+     *
+     * @return true if this card is a murder card; otherwise, false.
+     */
+    public boolean getIsMurder() {
+        return isMurder;
+    }
 
-  public String toString()
-  {
-    String res = String.format("[name: %s | type: %s | owner: %s | murder: %s]", getName(), getType(), getOwner(), getIsMurder());
-    return res;
-  }
+    /**
+     * Gets the player who holds this card.
+     *
+     * @return The player who owns this card.
+     */
+    public Player getOwner() {
+        return owner;
+    }
+
+    /**
+     * Gets the name of the card.
+     *
+     * @return The name of the card.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the type or category of the card.
+     *
+     * @return The type of the card.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Returns a string representation of the card, including its name, type, owner, and murder status.
+     *
+     * @return A string representing the card.
+     */
+    public String toString() {
+        return String.format("[name: %s | type: %s | owner: %s | murder: %s]", getName(), getType(), getOwner(), getIsMurder());
+    }
 }
